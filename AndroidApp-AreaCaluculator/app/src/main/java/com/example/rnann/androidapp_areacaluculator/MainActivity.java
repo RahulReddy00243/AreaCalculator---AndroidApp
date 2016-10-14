2c58032acbd5c4d91b5252eabd417feb3969c8a9
RahulReddyNannuru <rnannuru@gmail.com>
2016-10-14 05:32:05 -0400

    Declared view variables globally

--- AndroidApp-AreaCaluculator/app/src/main/java/com/example/rnann/androidapp_areacaluculator/MainActivity.java
+++ AndroidApp-AreaCaluculator/app/src/main/java/com/example/rnann/androidapp_areacaluculator/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.rnann.androidapp_areacaluculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declaring the view variables globally -> to make life easier!!
+
+    EditText lengthOneValueEditText;
+    EditText lengthTwoValueEditText;
+
+    TextView resultValueTextView;
+
+    Button triangle;
+    Button square;
+    Button Circle;
+    Button rectangle;
+    Button clear;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+
+        //Programmatically accessing the elements via their id located in R file!!
+
+        lengthOneValueEditText = (EditText) findViewById(R.id.Length1EditText);
+
+
+
+
+
+
     }
 }
