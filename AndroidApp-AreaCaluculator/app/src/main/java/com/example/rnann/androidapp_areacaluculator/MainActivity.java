f1c7fb3c1f8dcb62c275da13698c4bc53cdcaaaf
RahulReddyNannuru <rnannuru@gmail.com>
2016-10-14 05:51:00 -0400

    Implemeted OnClick Listener on Triangle Button inside which we read the input values given by the user and then printed them on logcat to confirm

--- AndroidApp-AreaCaluculator/app/src/main/java/com/example/rnann/androidapp_areacaluculator/MainActivity.java
+++ AndroidApp-AreaCaluculator/app/src/main/java/com/example/rnann/androidapp_areacaluculator/MainActivity.java
@@ -2,13 +2,15 @@ package com.example.rnann.androidapp_areacaluculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Declaring the view variables globally -> to make life easier!!
+    // Declaring the view variables globally -> to make life easier
 
     EditText lengthOneValueEditText;
     EditText lengthTwoValueEditText;
@@ -21,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     Button rectangle;
     Button clear;
 
+    String length1;
+    String length2;
+
     // Done With Declaration!!
 
     @Override
@@ -29,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main_activity);
 
 
-        //Programmatically accessing the elements via their id located in R file!!
+        //Programmatically accessing the elements via their id located in R file
 
         lengthOneValueEditText = (EditText) findViewById(R.id.Length1EditText);
         lengthTwoValueEditText = (EditText) findViewById(R.id.Length2EditText);
@@ -43,6 +48,33 @@ public class MainActivity extends AppCompatActivity {
         clear = (Button) findViewById(R.id.ButtonClearAll);
 
         // Done with Accessing!!
+
+
+        triangle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Reading the input length values
+
+                length1 = lengthOneValueEditText.getText().toString();
+                length2 = lengthTwoValueEditText.getText().toString();
+
+                //Done reading Input values entered by the User!!
+
+
+                //printing the values read to logcat to check functioning of the App
+
+                Log.d("rahul","length1 is :"+length1);
+                Log.d("rahul","length2 is :"+length2);
+
+                // Done checking, Values got printed  in the LogCat!!
+
+
+
+            }
+        });
+
+
 
 
 
